30b8a3e50f642ce0cae3d6d010145313997adeca
sreya123448 <sreya7930@gmail.com>
2025-07-26 13:29:34 +0530

    Fix address field for MySQL compatibility

--- Book_Stall/settings.py
+++ Book_Stall/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'Book_Stall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -98,19 +98,19 @@ WSGI_APPLICATION = 'Book_Stall.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sreya2002$bookstall',
-        'USER': 'sreya2002',
-        'PASSWORD': 'mysqlroot1234',
-        'HOST': 'sreya2002.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sreya2002$bookstall',
+#         'USER': 'sreya2002',
+#         'PASSWORD': 'mysqlroot1234',
+#         'HOST': 'sreya2002.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 
 
