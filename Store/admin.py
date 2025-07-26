4a6d1a86223ac58890365fe6e06372b50d57883a
sreya123448 <sreya7930@gmail.com>
2025-07-26 16:55:32 +0530

    add

--- Store/admin.py
+++ Store/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Book, Category
 from django.utils.html import format_html
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -21,3 +24,18 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author', 'publisher')
     list_editable = ('stock','available',)
     filter_horizontal = ('categories',)
+    readonly_fields = ('available',)
+
+    def save_model(self, request, obj, form, change):
+        # Auto-set available based on stock
+        obj.available = obj.stock > 0
+
+        # Validate offer
+        if obj.is_offer:
+            if not obj.original_price:
+                raise ValidationError("Original price must be set if the book is on offer.")
+            if obj.original_price <= obj.price:
+                raise ValidationError("Original price must be greater than the offer price.")
+            if obj.offer_expiry and obj.offer_expiry <= timezone.now():
+                raise ValidationError("Offer expiry must be in the future.")
+        super().save_model(request, obj, form, change)
